1ab4fd24e61fb59bab2d2a742025ee101a49f101
DumDereDum <46279571+DumDereDum@users.noreply.github.com>
2019-02-19 20:11:36 +0300

    Update hw2.c

--- hw2.c
+++ hw2.c
@@ -1,5 +1,3 @@
-// пока копипиздинг у Коли
-
 /*1.Программа. Дан текстовый файл. Написать функцию поиска самого
  короткого слова в строке (строки читать из данного файла. Найденное
  слово записать в новый файл. Имена обоих файлов задаются в командной строке*/
@@ -10,52 +8,57 @@
 #include <stdbool.h>
 
 
-
-void ShortestWord(char * file1, char * file2)
+void ShortesWord(char file1[], char file2[])
 {
-    unsigned long  min=1000;
-    unsigned long  len=1000;
-    char * str=NULL;
+    unsigned long min=1000;
+    unsigned long len=1000;
+    
     char * string[len];
+    char * str=NULL;
     
-
-    FILE * OpFile;
-    OpFile=fopen(file1,"r");
+    FILE * file;
     
-    if (OpFile==NULL)
+    file = fopen(file1, "r");
+    
+    if (file==NULL)
     {
         perror ("Error: can not read file");
         exit (EXIT_FAILURE);
     }
     
-    getline(&str,&len,OpFile);
+    getline(&str, &len, file);
     
-    for(char *p=strtok(str," "); p; p = strtok(NULL, " "))
+    for(char *word=strtok(str," "); word; word= strtok(NULL, " "))
     {
-        if(strlen(p)<min)
+        if(srtlen(word)< min)
         {
-            min = strlen(p);
-            strcpy(string,p);
+            min = strlen(word);
+            strcpy(string,word);
         }
     }
     
-    fclose(OpFile);
+    fclose(file);
     
-    OpFile = fopen(file2, "w");
+    file = fopen(file2, "w");
     
-    if (OpFile==NULL)
+    if (file==NULL)
     {
         perror ("Error: can not read file");
         exit (EXIT_FAILURE);
     }
     
-    fputs(string, OpFile);
-    fclose(OpFile);
+    fputs(string,file);
+    fclose(file);
     
 }
 
-int main(int arg, char * argv[])
+
+int main()
 {
-    ShortestWord(argv[1], argv[2]);
+    char file1[]="test1";
+    char file2[]="test2";
+    ShortestWord(file1,file2);
+    
     return 0;
 }
+
